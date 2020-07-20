17fe4e6f5bf9a307319b3f6ddf9ebdfc5215d118
Will Harris <mosinu@gmail.com>
2020-07-20 19:58:36 -0400

    added my afk bot

---
+++ afk-work.py
@@ -0,0 +1,40 @@
+# Python script to hide when I am idle.
+import pyautogui as pag
+import time
+import threading
+from time import sleep
+from random import randint
+
+x = 50   #how many times to run
+startx = 703
+starty = 731
+
+#print current position
+print(pag.position())
+while True:
+	pag.click(startx, starty,2)
+	pag.typewrite(".")
+	pag.typewrite(["enter"])
+	pag.typewrite(".")
+	pag.typewrite(["enter"])
+	print(pag.position())
+	sleep(60)
+	pag.moveTo(startx, starty,duration=.5)
+	pag.typewrite(".")
+	pag.typewrite(["enter"])
+	print(pag.position())
+	sleep(60)
+	pag.moveTo(startx, starty,duration=.5)
+	pag.typewrite(".")
+	pag.typewrite(["enter"])
+	print(pag.position())
+	pag.click(startx, starty,2)
+	pag.typewrite(".")
+	pag.typewrite(["enter"])
+
+	startx = startx + 1
+	starty = starty + 1
+
+	x = x - 1
+	if x == 0:
+		break
